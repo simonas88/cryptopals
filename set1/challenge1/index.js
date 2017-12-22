cf69ed4c4871d03c4452a45befe0bd155556bed3
Simonas Drungilas <simonas.drungilas@devbridge.com>
2017-12-22 16:54:56 +0200

    Add function to convert 8 bit integer array into 6 bit integer array

--- set1/challenge1/index.js
+++ set1/challenge1/index.js
@@ -1,13 +1,64 @@
 function getIntsFromHex(input) {
   const array = [];
 
-  for (let i = 1; i <= input.length; i += 2) {
+  for (let i = 1; i < input.length; i += 2) {
     array.push(parseInt(input.substring(i - 1, i + 1), 16));
   }
 
   return array;
 }
 
+function convertToSixBits(input) {
+  const inputLength = input.length;
+  const pad = inputLength % 3;
+  const fullThrees = inputLength - pad;
+
+  const result = [];
+
+  for (let i = 2; i < fullThrees; i += 3) {
+    const b1 = input[i - 2];
+    const b2 = input[i - 1];
+    const b3 = input[i];
+
+
+    const s1 = b1 >>> 2;
+    const s2 = (b1 << (6 + 24) >>> (2 + 24)) | (b2 >>> 4);
+    const s3 = (b2 << (4 + 24) >>> (2 + 24)) | (b3 >>> 6);
+    const s4 = b3 << (2 + 24) >>> (2 + 24);
+
+    result.push(s1);
+    result.push(s2);
+    result.push(s3);
+    result.push(s4);
+  }
+
+  if (pad === 2) {
+    const b1 = input[inputLength - 2];
+    const b2 = input[inputLength - 1];
+
+    const s1 = b1 >>> 2;
+    const s2 = (b1 << (6 + 24) >>> (2 + 24)) | (b2 >>> 4);
+    const s3 = (b2 << (4 + 24) >>> (2 + 24));
+
+    result.push(s1);
+    result.push(s2);
+    result.push(s3);
+  }
+
+  if (pad === 1) {
+    const b1 = input[inputLength - 1];
+
+    const s1 = b1 >>> 2;
+    const s2 = (b1 << (6 + 24) >>> (2 + 24));
+
+    result.push(s1);
+    result.push(s2);
+  }
+
+  return result;
+}
+
 module.exports = {
-  getIntsFromHex
+  getIntsFromHex,
+  convertToSixBits,
 }
--- set1/challenge1/spec.js
+++ set1/challenge1/spec.js
@@ -1,5 +1,6 @@
 const {
   getIntsFromHex,
+  convertToSixBits,
 } = require('./index');
 
 describe('Set 1 specs', () => {
@@ -9,5 +10,29 @@ describe('Set 1 specs', () => {
     const expectedOutput = [ 0b01001111, 0b01100010 ];
 
     expect(getIntsFromHex(input)).toEqual(expectedOutput);
+  });
+
+  it('should convert three bytes into four bytes', () => {
+    const input = [ 0b11001100, 0b11101110, 0b01110111 ];
+
+    const expectedOutput = [ 0b00110011, 0b00001110, 0b00111001, 0b00110111 ];
+
+    expect(convertToSixBits(input)).toEqual(expectedOutput);
+  });
+
+  it('should convert two bytes into three bytes', () => {
+    const input = [ 0b11001100, 0b11101110 ];
+
+    const expectedOutput = [ 0b00110011, 0b00001110, 0b00111000 ];
+
+    expect(convertToSixBits(input)).toEqual(expectedOutput);
+  });
+
+  it('should convert a byte into two bytes', () => {
+    const input = [ 0b11001100 ];
+
+    const expectedOutput = [ 0b00110011, 0b00000000 ];
+
+    expect(convertToSixBits(input)).toEqual(expectedOutput);
   });
 });
