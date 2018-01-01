d3a9aa5fc48c31479c59c063d93bf527cba55b3a
Simonas Drungilas <simonas.drungilas@devbridge.com>
2018-01-01 21:19:25 +0200

    Chal4: Add method to read a file to memory and parse strings

--- set1/challenge4/index.js
+++ set1/challenge4/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+const { deCipher } = require('../challenge3');
+
 function readTextToArray(inputPath) {
   const readPromise = new Promise((resolve, reject) =>
     fs.readFile(inputPath, 'utf8', (err, data) => err ? reject(err) : resolve(data)));
@@ -7,6 +9,23 @@ function readTextToArray(inputPath) {
   return readPromise.then(data => data.split('\n'));
 }
 
+async function readStrings(inputPath) {
+  const array = await readTextToArray(inputPath);
+
+  const result = array.reduce((agg, element) => {
+    const deciphered = deCipher(element);
+
+    if(deciphered.score > agg.score) {
+      return deciphered;
+    }
+
+    return agg;
+  }, { score: 0, message: '', cipher: 0 });
+
+  return result;
+}
+
 module.exports = {
   readTextToArray,
+  readStrings
 };
--- set1/challenge4/spec.js
+++ set1/challenge4/spec.js
@@ -1,5 +1,6 @@
 const {
   readTextToArray,
+  readStrings,
 } = require('./index');
 
 describe('Set 1 specs', () => {
@@ -8,7 +9,16 @@ describe('Set 1 specs', () => {
       const expectedOutput = '581e0829214202063d70030845e5301f5a5212ed0818e22f120b211b171b';
 
       const result = await readTextToArray('./set1/challenge4/input.txt');
+
       expect(result[6]).toEqual(expectedOutput);
+    });
+
+    it('Should find the encoded string and decipher it', async () => {
+      const result = await readStrings('./set1/challenge4/input.txt');
+
+      const expectedResult = 'Now that the party is jumping\n';
+
+      expect(result.message).toEqual(expectedResult);
     });
   });
 });
