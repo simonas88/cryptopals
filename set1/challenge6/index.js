c0071a44913a86d94888eb9cc5713c50902b11e0
Simonas Drungilas <simonas.drungilas@devbridge.com>
2018-01-02 18:10:58 +0200

    Chal6: Add debasifier64

--- set1/challenge6/index.js
+++ set1/challenge6/index.js
@@ -1,5 +1,16 @@
 const { stringToIntArray } = require('../challenge5');
 
+const B64_TABLE = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+  'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+  'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+  'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+  'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+  'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+  'w', 'x', 'y', 'z', '0', '1', '2', '3',
+  '4', '5', '6', '7', '8', '9', '+', '/',
+];
+
 function getHammingDistance(input1, input2) {
   if (input1.length !== input2.length) {
     throw Error('unequal length inputs');
@@ -15,6 +26,47 @@ function getHammingDistance(input1, input2) {
   }, 0);
 }
 
+function getIntsFromB64(input) {
+  //const sixBitInts = input.split('').filter(char => char !== '=').map(char => B64_TABLE.indexOf(char));
+
+  const array = input.split('').reduce((agg, char, index) => {
+    if (char === '=') { return agg; }
+    
+    const six = B64_TABLE.indexOf(char);
+
+    const position = index % 4;
+
+    if (position === 0) {
+      agg.push(six << 2);
+    }
+
+    if (position === 1) {
+      agg.splice(agg.length - 1, 1, ( agg[agg.length - 1] | six >>> 4 ));
+      agg.push(six << (24 + 4) >>> 24);
+    }
+
+    if (position === 2) {
+      agg.splice(agg.length - 1, 1, ( agg[agg.length - 1] | six >>> 2 ));
+      agg.push(six << (24 + 6) >>> 24);
+    }
+
+    if (position === 3) {
+      agg.splice(agg.length - 1, 1, ( agg[agg.length - 1] | six ));
+    }
+
+    return agg;
+  }, []);
+
+  if (array[array.length - 1] === 0) {
+    array.splice(-1);
+  }
+
+  return array;
+}
+
+//MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMQ==
+
 module.exports = {
   getHammingDistance,
+  getIntsFromB64,
 };
--- set1/challenge6/spec.js
+++ set1/challenge6/spec.js
@@ -1,5 +1,6 @@
 const {
   getHammingDistance,
+  getIntsFromB64,
 } = require('./index');
 
 describe('Set1 specs', () => {
@@ -15,6 +16,30 @@ describe('Set1 specs', () => {
 
     it('Should throw an error, because input strings differ in length', () => {
       expect(() => getHammingDistance('foo', 'fo')).toThrow('unequal length inputs');
+    });
+
+    it('Should return int array given BASE64 string', () => {
+      const input = 'zO53';
+
+      const expectedOutput = [ 0b11001100, 0b11101110, 0b01110111 ];
+
+      expect(getIntsFromB64(input)).toEqual(expectedOutput);
+    });
+
+    it('Should return int array given BASE64 string', () => {
+      const input = 'zO4=';
+
+      const expectedOutput = [ 0b11001100, 0b11101110 ];
+
+      expect(getIntsFromB64(input)).toEqual(expectedOutput);
+    });
+
+    it('Should return int array given BASE64 string', () => {
+      const input = 'zA==';
+
+      const expectedOutput = [ 0b11001100 ];
+
+      expect(getIntsFromB64(input)).toEqual(expectedOutput);
     });
   });
 });
